945d65cc22bf04441375c22f0a6b07854fdafc3d
Jason Feinstein <jason.feinstein@gmail.com>
2020-12-17 20:04:13 -0800

    Day 18 stub

--- src/main/kotlin/us/jwf/aoc2020/AdventOfCode2020.kt
+++ src/main/kotlin/us/jwf/aoc2020/AdventOfCode2020.kt
@@ -19,5 +19,6 @@ class AdventOfCode2020 : BaseAdventOfCode(
   ::Day14DockingData,
   ::Day15RambunctiousRecitation,
   ::Day16TicketTranslation,
-  ::Day17ConwayCubes
+  ::Day17ConwayCubes,
+  ::Day18
 )
