b0cce99ebf4b0796b00711bfdc139db756d1c219
Jason Feinstein <jwf@google.com>
2021-12-22 14:16:00 -0800

    Add solution to 2015 day 24

--- src/main/kotlin/us/jwf/aoc2015/Day24HangsInTheBalance.kt
+++ src/main/kotlin/us/jwf/aoc2015/Day24HangsInTheBalance.kt
@@ -1,96 +1,120 @@
 package us.jwf.aoc2015
 
 import java.io.Reader
-import java.util.PriorityQueue
+import java.util.LinkedList
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
 import us.jwf.aoc.Day
-import us.jwf.aoc.product
 import us.jwf.aoc.toIntFlow
 
 /**
  * AoC 2015 - Day 24
  */
-class Day24HangsInTheBalance : Day<ULong, Int> {
+class Day24HangsInTheBalance : Day<ULong, ULong> {
   override suspend fun executePart1(input: Reader): ULong {
-    val weights = input.toIntFlow().toList()
+    val weights = input.toIntFlow().map { it.toULong() }.toList()
     val total = weights.sum()
-    val target = total / 3
+    val target = total / 3uL
 
-    val start = Arrangement(weights.toSet())
-    val queue = PriorityQueue<Arrangement> { a, b -> a compareTo b }
-      .also { it.offer(start) }
-    val visited = mutableSetOf(start)
+    val start = Arrangement(otherGroupMembers = weights, otherGroupSum = total)
 
-    val results = mutableSetOf<ULong>()
+    val queue = LinkedList<Arrangement>().apply { add(start) }
+    val visited = mutableSetOf<Arrangement>()
+
     while (queue.isNotEmpty()) {
       val next = queue.poll()
+      if (next.isDone(target)) return next.group1Product
 
-      if (next.group1Sum == target && next.group2Sum == target && next.group3Sum == target) {
-        results.add(next.entanglement)
-        println(next.entanglement)
-      }
-
-      next.remaining.forEach { wt ->
-        val newRemaining = next.remaining - wt
-        next.addToGroup1(wt, newRemaining, target)?.takeIf { it !in visited }?.let {
-          visited += it
-          queue.offer(it)
+      next.nextOptions(target)
+        .forEach {
+          if (it in visited) return@forEach
+          queue.add(it)
+          visited.add(it)
         }
-        next.addToGroup2(wt, newRemaining, target)?.takeIf { it !in visited }?.let {
-          visited += it
-          queue.offer(it)
-        }
-        next.addToGroup3(wt, newRemaining, target)?.takeIf { it !in visited }?.let {
-          visited += it
-          queue.offer(it)
-        }
-      }
     }
 
-    return results.minOf { it }
+    return 0uL
   }
 
-  override suspend fun executePart2(input: Reader): Int {
-    TODO("Not yet implemented")
+  override suspend fun executePart2(input: Reader): ULong {
+    val weights = input.toIntFlow().map { it.toULong() }.toList()
+    val total = weights.sum()
+    val target = total / 4uL
+
+    val start = Arrangement(otherGroupMembers = weights, otherGroupSum = total)
+
+    val queue = LinkedList<Arrangement>().apply { add(start) }
+    val visited = mutableSetOf<Arrangement>()
+
+    while (queue.isNotEmpty()) {
+      val next = queue.poll()
+      if (next.isDonePart2(target)) return next.group1Product
+
+      next.nextOptions(target)
+        .forEach {
+          if (it in visited) return@forEach
+          queue.add(it)
+          visited.add(it)
+        }
+    }
+
+    return 0uL
   }
 
   data class Arrangement(
-    val remaining: Set<Int>,
-    val group1Count: Int = 0,
+    val group1Sum: ULong = 0uL,
     val group1Product: ULong = 1uL,
-    val group1Sum: Int = 0,
-    val group2Sum: Int = 0,
-    val group3Sum: Int = 0,
-  ) : Comparable<Arrangement> {
-    val entanglement: ULong = group1Product
-
-    fun isLegal(target: Int): Boolean = group1Sum <= target && group2Sum <= target && group3Sum <= target
-
-    fun addToGroup1(weight: Int, newRemaining: Set<Int>, target: Int): Arrangement? {
-      return Arrangement(
-        newRemaining,
-        group1Count + 1,
-        group1Product * weight.toULong(),
-        group1Sum + weight,
-        group2Sum,
-        group3Sum
-      ).takeIf { it.isLegal(target) }
+    val otherGroupMembers: List<ULong>,
+    val otherGroupSum: ULong,
+  ) {
+    fun nextOptions(target: ULong): List<Arrangement> {
+      return otherGroupMembers.mapNotNull { member ->
+        if (group1Sum + member > target) return@mapNotNull null
+        Arrangement(
+          group1Sum + member,
+          group1Product * member,
+          otherGroupMembers - member,
+          otherGroupSum - member
+        )
+      }
     }
 
-    fun addToGroup2(weight: Int, newRemaining: Set<Int>, target: Int): Arrangement? {
-      return copy(remaining = newRemaining, group2Sum = group2Sum + weight).takeIf { it.isLegal(target) }
+    fun isDone(groupSize: ULong): Boolean {
+      if (group1Sum != groupSize || otherGroupMembers.sum() != groupSize * 2uL) return false
+
+      // Check that otherGroupMembers can be split into even weights.
+      fun canSplit(remainingTotal: ULong, remainingOptions: Set<ULong>): Boolean {
+        if (remainingTotal == 0uL) return true
+        val possibleOptions = remainingOptions.filter { it <= remainingTotal }
+        if (possibleOptions.isEmpty()) return false
+
+        return possibleOptions.any { canSplit(remainingTotal - it, remainingOptions - it) }
+      }
+
+      // only works if they're all different, if they were the same we'd need a map of counts.
+      return canSplit(groupSize, otherGroupMembers.toSet())
     }
 
-    fun addToGroup3(weight: Int, newRemaining: Set<Int>, target: Int): Arrangement? {
-      return copy(remaining = newRemaining, group3Sum = group3Sum + weight).takeIf { it.isLegal(target) }
-    }
+    fun isDonePart2(groupSize: ULong): Boolean {
+      if (group1Sum != groupSize || otherGroupMembers.sum() != groupSize * 3uL) return false
 
-    override operator fun compareTo(other: Arrangement): Int {
-      val totalSum = group1Sum + group2Sum + group3Sum
-      val otherTotalSum = other.group1Sum + other.group2Sum + other.group3Sum
-      if (totalSum != otherTotalSum) return otherTotalSum - totalSum
-      if (group1Count != other.group1Count) return group1Count - other.group1Count
-      return entanglement compareTo other.entanglement
+      // Check that otherGroupMembers can be split into even weights.
+      val cache = mutableMapOf<Triple<ULong, ULong, Set<ULong>>, Boolean>()
+      fun canSplit(remainingInA: ULong, remainingInB: ULong, remainingOptions: Set<ULong>): Boolean {
+        val args = Triple(remainingInA, remainingInB, remainingOptions)
+        cache[args]?.let { return it }
+
+        if (remainingInA == 0uL && remainingInB == 0uL) return true
+        val possibleAOptions = remainingOptions.filter { it <= remainingInA }
+        val possibleBOptions = remainingOptions.filter { it <= remainingInB }
+
+        return (possibleAOptions.any { canSplit(remainingInA - it, remainingInB, remainingOptions - it) } ||
+          possibleBOptions.any { canSplit(remainingInA, remainingInB - it, remainingOptions - it) })
+          .also { cache[args] = it }
+      }
+
+      // only works if they're all different, if they were the same we'd need a map of counts.
+      return canSplit(groupSize, groupSize, otherGroupMembers.toSet())
     }
   }
 }
